84a75d108530ccb0dab8788966547bf254febe92
ConcepcionArequipa <concepcion.arequipa@epn.edu.ec>
2025-12-21 23:41:23 -0500

    Tarea Java Swing-Simulador Bancario Interactivo Actualizado

--- Simulador_Bancario_Interactivo_BD/conexion/ConexionBD.java
+++ Simulador_Bancario_Interactivo_BD/conexion/ConexionBD.java
@@ -1,25 +1,23 @@
 package conexion;
-import conexion.ConexionBD;
+
 import java.sql.*;
 public class ConexionBD {
     private static final String url="jdbc:mysql://ureaxaqehn5mna3l:nw5M3xD87dmVWmKIY0Bx@brpqmkk9qgvixttn6eum-mysql.services.clever-cloud.com:3306/brpqmkk9qgvixttn6eum";
     private static final String user="ureaxaqehn5mna3l";
     private static final String password="nw5M3xD87dmVWmKIY0Bx";
+    //Metodo que debe de retornar una Connection si la conexion es correcta
     public static Connection getConexion(){
-        Connection conexion=null;
+
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conexion = DriverManager.getConnection(url, user, password);
+            Connection conexion = DriverManager.getConnection(url, user, password);
             System.out.println("Conexión exitosa a la base de datos");
-        }
-        catch(ClassNotFoundException e){
-            System.out.println("Error: Driver no encontrado");
+            return conexion;
         }
 
         catch(SQLException e){
-            System.out.println("Error de conexión: " + e.getMessage());
+            throw new RuntimeException("Error de conexión: " + e.getMessage());
         }
-        return conexion;
+
     }
 
 
